c33892f152a4bfcf80a2dfd5e6125c3df024d1d9
Henry <liuhanning8618@gmail.com>
2025-04-05 14:02:47 +0800

    First Try Vercel.

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { gsap } from "gsap";
+
+const menuBtn = document.querySelector(".menu_icon");
+const closeBtn = document.querySelector(".close_menu");
+
+
+let t1 = gsap.timeline({paused: true});
+t1.to(".menu", {opacity: 1, duration: 1, top: 0});
+t1.to(".nav", {opacity: 1, duration: 1, stagger: 0.2, marginTop: 0}, ">-0.5");
+
+menuBtn.addEventListener("click", () => {
+  t1.timeScale(1);
+  t1.play();
+});
+
+closeBtn.addEventListener("click", () => {
+  t1.timeScale(2);
+  t1.reverse();
+});
