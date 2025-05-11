eb81318be9fb4b256b63a379f20a2d6f7b275e70
Hanning-Liu <liuhanning8618@gmail.com>
2025-05-11 17:22:31 +0800

    Align the close btn with menu btn

--- src/main.js
+++ src/main.js
@@ -8,12 +8,20 @@ let t1 = gsap.timeline({paused: true});
 t1.to(".menu", {opacity: 1, duration: 1, top: 0});
 t1.to(".nav", {opacity: 1, duration: 1, stagger: 0.2, marginTop: 0}, ">-0.5");
 
-menuBtn.addEventListener("click", () => {
+menuBtn.addEventListener("click", handleMenuClick);
+menuBtn.addEventListener("touchstart", handleMenuClick, {passive: true});
+
+closeBtn.addEventListener("click", handleCloseClick);
+closeBtn.addEventListener("touchstart", handleCloseClick, {passive: true});
+
+function handleMenuClick(e) {
+  e.preventDefault();
   t1.timeScale(1);
   t1.play();
-});
+}
 
-closeBtn.addEventListener("click", () => {
+function handleCloseClick(e) {
+  e.preventDefault();
   t1.timeScale(2);
   t1.reverse();
-});
+}
